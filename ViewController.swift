0645efe60b99872cc90c1ac03b6d38c0f44c226e
Pratik <pratik1785@gmail.com>
2017-05-04 21:22:21 +1000

    no message

---
+++ ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Swift first App
+//
+//  Created by Pratik Chauhan on 29/4/17.
+//  Copyright © 2017 Pratik Chauhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var thelabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func ButtonTapped(_ sender: Any) {
+        
+        tapCount = tapCount + 1
+        
+        if tapCount >= 10 {
+        thelabel.text = "you have tapped the button 10 times!"
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        thelabel.text = "Hello There"
+            }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
